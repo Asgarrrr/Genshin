2c13b266ac42b3484f4ac1e286bddf65a653560b
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2020-12-15 09:53:13 +0100

    first commit

---
+++ PHP/DB.php
@@ -0,0 +1,8 @@
+<?php
+    try {
+        $dbh = new PDO('mysql:host=mysql-genshin.alwaysdata.net;dbname=genshin_db', "genshin", "23w9j423w9j4");
+    } catch (PDOException $e) {
+        print "Erreur ! — " . $e->getMessage() . "<br/>";
+        die();
+    }
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    include("PHP/DB.php");
+?>
+
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Genshin — Database</title>
+    </head>
+    <body>
+        <a href="Pages/Character.php">test</a>
+    </body>
+</html>
