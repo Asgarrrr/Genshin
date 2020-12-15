3c89c660bb3df8c0ec1576eacad11a7b2207c2b0
Jérémy Caruelle <36574651+Asgarrrr@users.noreply.github.com>
2020-12-15 12:12:06 +0100

    Change : Switch between local and online database.

--- PHP/DB.php
+++ PHP/DB.php
@@ -1,6 +1,7 @@
 <?php
     try {
-        $dbh = new PDO('mysql:host=mysql-genshin.alwaysdata.net;dbname=genshin_db', "genshin", "23w9j423w9j4");
+        //  $dbh = new PDO('mysql:host=mysql-genshin.alwaysdata.net;dbname=genshin_db', "genshin", "23w9j423w9j4");
+        $dbh = new PDO('mysql:localhost;dbname=Genshin', "root", "23w9j4");
     } catch (PDOException $e) {
         print "Erreur ! — " . $e->getMessage() . "<br/>";
         die();
