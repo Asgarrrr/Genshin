a322c7f16d8c4ecfe8906227a88ed26666d2876c
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2020-12-15 17:08:27 +0100

    FIX: Im débilus.

--- PHP/DB.php
+++ PHP/DB.php
@@ -1,7 +1,7 @@
 <?php
     try {
-        //  $dbh = new PDO('mysql:host=mysql-genshin.alwaysdata.net;dbname=genshin_db', "genshin", "23w9j423w9j4");
-        $dbh = new PDO('mysql:localhost;dbname=Genshin', "root", "23w9j4");
+        //$dbh = new PDO('mysql:host=mysql-genshin.alwaysdata.net;dbname=genshin_db', "genshin", "23w9j423w9j4");
+        $dbh = new PDO('mysql:host=localhost;dbname=genshin_db', "root", "root");
     } catch (PDOException $e) {
         print "Erreur ! — " . $e->getMessage() . "<br/>";
         die();
