ec389c8c8f66eae90b6e5150089ceb803dc7dafc
Jérémy Caruelle <36574651+Asgarrrr@users.noreply.github.com>
2020-12-15 12:12:53 +0100

    Open : First version of bbd information retrieval

--- Pages/Character.php
+++ Pages/Character.php
@@ -1,5 +1,24 @@
+<?php
+
+    try {
+        include("../PHP/DB.php");
+
+
+        $stmt = $dbh->prepare("SELECT * FROM Characters WHERE Name= :name");
+        $stmt->execute(['name' => $_GET["Name"]]); 
+        $user = $stmt->fetch();
+
+        print_r($user);
+
+    } catch (PDOException $e) {
+        print "Erreur ! — " . $e->getMessage() . "<br/>";
+    }
+
+?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,9 +29,9 @@
 <body>
 
     <header class="cha">
-        <img src="../Assets/Characters/Jean.png" alt="" class="back">
-        <h1>JEAN</h1>
-        <img src="../Assets/Characters/Jean.png" alt="" class="front">
+        <img src="../Assets/Characters/Xiao.png" alt="" class="back">
+        <h1>Xiao</h1>
+        <img src="../Assets/Characters/Xiao.png" alt="" class="front">
     </header>
 
 </body>
