5c8766a511302a937ec1ed0c28698c8a9e81d81c
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2020-12-30 16:03:09 +0100

    Update Weapon.php

--- Pages/Weapon.php
+++ Pages/Weapon.php
@@ -72,7 +72,7 @@
                     <ul class="navbar-nav mb-2 mb-lg-0">
 
                         <li class="nav-item">
-                            <a class="nav-link" href="Characters.php">Characters</a>
+                            <a class="nav-link" href="../">Characters</a>
                         </li>
 
                         <li class="nav-item">
