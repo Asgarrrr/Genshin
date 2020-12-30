eb5508ecf99d8103ae0c0626b108f9a8dfc6c907
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2020-12-30 16:08:32 +0100

    Update DB.php

--- PHP/DB.php
+++ PHP/DB.php
@@ -1,9 +1,24 @@
 <?php
     try {
-        //$dbh = new PDO('mysql:host=mysql-genshin.alwaysdata.net;dbname=genshin_db', "genshin", "23w9j423w9j4");
-        $dbh = new PDO('mysql:host=localhost;dbname=genshin_db', "root", "root");
+        $dbh = new PDO('mysql:host=mysql-genshin.alwaysdata.net;dbname=genshin_db', "genshin", "23w9j423w9j4");
+        //$dbh = new PDO('mysql:host=localhost;dbname=genshin_db', "root", "root");
     } catch (PDOException $e) {
         print "Erreur ! — " . $e->getMessage() . "<br/>";
         die();
+    }
+
+
+    function group_by($key, $data) {
+        $result = array();
+
+        foreach($data as $val) {
+            if(array_key_exists($key, $val)){
+                $result[$val[$key]][] = $val;
+            }else{
+                $result[""][] = $val;
+            }
+        }
+
+        return $result;
     }
 ?>
