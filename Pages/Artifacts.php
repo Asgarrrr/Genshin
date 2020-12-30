be34adb69723c0c0b847c2f0be60adc3c5846817
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2020-12-30 16:03:06 +0100

    Update Artifacts.php

--- Pages/Artifacts.php
+++ Pages/Artifacts.php
@@ -38,7 +38,7 @@
                 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <ul class="navbar-nav mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="Character.php">Characters</a>
+                            <a class="nav-link" href="../">Characters</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="Weapon.php">Weapons</a>
